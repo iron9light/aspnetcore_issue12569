4a1a6a3ba1e5881e6524b0ccbcddf78af4623bec
iron9light <iron9light@gmail.com>
2019-07-25 23:01:37 +0800

    init

---
+++ aspnetcorebug/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcorebug.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<ValueB>> Get()
+        {
+            var valueAs = Enumerable.Range(0, 10).Select(x => new ValueA { Value = x }).ToList();
+            return new ActionResult<IEnumerable<ValueB>>(valueAs.Select(x => new ValueB { Value = x.Value }));
+        }
+    }
+
+    public class ValueA
+    {
+        public int Value { get; set; }
+    }
+
+    public class ValueB
+    {
+        public int Value { get; set; }
+    }
+}
